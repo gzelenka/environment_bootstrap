567e7338ac9f76ea37d4f0caf158675a76323c6c
Geoff Zelenka <geoff.zelenka@rackspace.com>
2015-07-09 16:12:43 -0400

    Add a vimrc file and fix the bootstrap script

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -1,7 +1,11 @@
-#apt-get install vim curl git
+apt-get install vim curl git
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+pushd ~/.vim/autoload
+wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+popd
 
 pushd ~/.vim/bundle
 git clone https://github.com/scrooloose/syntastic.git
 popd
+
+cp vimrc-boiler ~/.vimrc
